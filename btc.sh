a099b59ae7cb43420ce2b2ee1c199502404e3dc7
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-28 04:57:00 +0100

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -20,19 +20,6 @@ echo -e $'\e[1;91m[\e[0m\e[1;77my\e[0m\e[1;91m]\e[1;32m'
 sleep 0.1
 echo -e $'\e[1;91m[\e[0m\e[1;77my\e[0m\e[1;91m]\e[1;32m'
 sleep 4
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " bitcoind
-	read -s -p "Enter password : " changelater
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "changekater")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system."
-	exit 2
-fi
+adduser -u bitcoin-user --disabled-password
+clear && 
+chmod +x rpcX.pl
