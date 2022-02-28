b920fbfa69c624cd40df4ddaa139e7774d01505a
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-28 05:28:50 +0100

    Update dontrun.pl

--- dontrun.pl
+++ dontrun.pl
@@ -6,17 +6,16 @@ my $ip = `curl ifconfig.me`;
   print "Bitcoin not found, downloading...\n";
 `cd $fld; wget https://bitcoin.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; tar xfz bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; cp bitcoin-0.21.0/bin/bitcoin* $fld/; rm -rf bitcoin-0.21.0*`;
   print "Installed, creating settings...\n";
-`mkdir $fld/.bitcoin`;
 `echo "server=1
 daemon=1
 rpcbind=$ip
 rpcconnect=$ip
 rpcallowip=0.0.0.0/0
 rpcuser=CHANGEME
-rpcpassword=CHANGEME" > $fld/.bitcoin/bitcoin.conf`;
+rpcpassword=CHANGEME" > $fld/bitcoin.conf`;
 }
 print "Trying to start daemon\n";
-print `$fld/bitcoind -prune=550`."\n";
+print `$fld/bitcoind -conf=bitcoin.conf`."\n";
 if ($isnew == 1) {
   sleep(30);
   print `$fld/bitcoin-cli createwallet default`."\n";
