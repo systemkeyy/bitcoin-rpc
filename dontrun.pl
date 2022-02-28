efc8ccc052f2edf75ce8c5080ec0c318bdded227
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-28 07:49:59 +0100

    Update dontrun.pl

--- dontrun.pl
+++ dontrun.pl
@@ -5,20 +5,18 @@ if (!-d "$fld/.bitcoin") {
   $isnew = 1;
   my $ip = `curl ifconfig.me`;
   print "Bitcoin not found, downloading...\n";
-`cd $fld; wget https://bitcoin.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; tar xfz bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; cp bitcoin-0.21.0/bin/bitcoin* $fld/; rm -rf bitcoin-0.21.0*`;
+`cd $fld/; wget https://bitcoin.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; tar xfz bitcoin-0.21.0-x86_64-linux-gnu.tar.gz; cp bitcoin-0.21.0/bin/bitcoin* $fld/; rm -rf bitcoin-0.21.0*`;
   print "Installed, creating settings...\n";
 `mkdir $fld/.bitcoin`;
-`echo "server=1
-daemon=1
+`echo "daemon=1
 rpcbind=$ip
 rpcconnect=$ip
-prune=550
 rpcallowip=0.0.0.0/0
-rpcuser=CHANGEME
-rpcpassword=CHANGEME" > $fld/.bitcoin/bitcoin.conf`;
+rpcuser=DPJidjo123safasfw
+rpcpassword=Ffdasaf1231saa" > $fld/.bitcoin/bitcoin.conf`;
 }
-print "Trying to start daemon\n";
-print `$fld/bitcoind -conf=/root/home/bitcoin-user/bitcoin.conf`."\n";
+print "Starting bitcoin daemon...\n";
+print `$fld/bitcoind -prune=550`."\n";
 if ($isnew == 1) {
   sleep(30);
   print `$fld/bitcoin-cli createwallet default`."\n";
@@ -26,5 +24,4 @@ if ($isnew == 1) {
   sleep(10);
   print `$fld/bitcoin-cli loadwallet default`."\n";
 }
-print "Bitcoin is live at $ip\n";
 print "!!REMEMBER TO CHANGE THE RPCUSER AND PASSWORD IN /.BITCOIN/BITCOIN.CONF!!!\n";
