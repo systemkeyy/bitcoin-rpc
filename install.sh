299738a6d731f3ffa1deeba988fb621d450ff3ba
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-02-28 07:46:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,45 @@ echo -e $'\e[1;91m[\e[0m\e[1;77my\e[0m\e[1;91m]\e[1;32m'
 sleep 0.1
 echo -e $'\e[1;91m[\e[0m\e[1;77my\e[0m\e[1;91m]\e[1;32m'
 sleep 4
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'Loading suited settings and creating bitcoin-user!'.';
+echo -ne '>                       [5%]\r'
+sleep 0.1
+echo -ne '>>                      [10%]\r'
+sleep 0.2
+echo -ne '>>>>                    [20%]\r'
+sleep 0.1
+echo -ne '>>>>>                   [25%]\r'
+sleep 0.1
+echo -ne '>>>>>>                  [30%]\r'
+sleep 0.1
+echo -ne '>>>>>>>                 [35%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>                [40%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>               [45%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>              [50%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>             [55%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>            [60%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>           [65%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>          [70%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>         [75%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>        [80%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>       [85%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>      [90%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>     [95%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>>>   [100%]\r.'
+sleep 2
 echo -e changethis changethis lets go now | adduser bitcoin-user
 sleep 2
 chmod +x dontrun.pl
